3a229e7d03a856cd0254a1e28633c95f56cacdd7
Gravity22222 <89166719+Gravity22222@users.noreply.github.com>
2025-10-23 17:25:46 -0300

    fix: Configurar H2 database para teste

--- src/test/java/br/com/eurofarma/ppm_api/PpmApiApplicationTests.java
+++ src/test/java/br/com/eurofarma/ppm_api/PpmApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.eurofarma.ppm_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PpmApiApplicationTests {
 
 	@Test
